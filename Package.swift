b09483344aa5c9e64faa6597e41c834896dd321a
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2024-12-09 16:41:35 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CYBUtilities",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CYBUtilities",
+            targets: ["CYBUtilities"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CYBUtilities"),
+        .testTarget(
+            name: "CYBUtilitiesTests",
+            dependencies: ["CYBUtilities"]
+        ),
+    ]
+)
